9528586f2ea45bd159618cfffa21e8911dd79bec
Hikarunnie <Kharabadze.Ani@kiu.edu.ge>
2025-12-25 23:43:26 +0400

    change to registerButton logic

--- src/main/java/ge/tbc/testautomation/data/Constants.java
+++ src/main/java/ge/tbc/testautomation/data/Constants.java
@@ -23,7 +23,7 @@ public class Constants {
     public static final String USER_EMAIL ="Hikarunnie@gmail.com";
     public static final String USER_PASSWORD ="Testing4";
     public static final String BROWSE_EVENT_URL ="http://34.52.231.181:4201/events";
-    public static final String EVENT_NAME = "Cybersecurity Essentials";
-    public static final String FOUND_EVENT_URL ="http://34.52.231.181:4201/events/108";
+    public static final String EVENT_NAME = "DevOps CI/CD Masterclass";
+    public static final String FOUND_EVENT_URL ="http://34.52.231.181:4200/events/112";
     public static final String ALREADY_REGISTERED_TXT ="Cancel Registration";
 }
